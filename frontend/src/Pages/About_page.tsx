ec5bd4501b240c3f8895a32ec4435bd708d56507
shrivanshu04 <shrivanshu4@gmail.com>
2025-08-06 11:27:21 +0530

    our journey

--- frontend/src/Pages/About_page.tsx
+++ frontend/src/Pages/About_page.tsx
@@ -4,6 +4,7 @@ import Header from '../Component/Header';
 import { ThemeProvider } from '../contexts/ThemeContext';
 import Static from '../Component/about_components/Static';
 import Gallery from '../Component/about_components/Gallery';
+import Ourjourney from '../Component/about_components/Ourjourney';
 
 
 
@@ -13,6 +14,7 @@ function About_page() {
       <div className="App bg-white dark:bg-gray-900 transition-colors duration-300 min-h-screen">
         <Header />
         <Static/>
+        <Ourjourney />
         <Gallery/>
         <Footer />
       </div>
