f6a5220cd7f21580f9bbd38007b90a45033acd27
amishadubey44 <amishadubey44@gmail.com>
2025-08-22 16:02:39 +0530

    CobotComparisionChart added with full responsivenss

--- frontend/src/Pages/Cobot_page.tsx
+++ frontend/src/Pages/Cobot_page.tsx
@@ -4,6 +4,7 @@ import CobotVariants from '../Component/Cobot_components/CobotVariants';
 import CobotDetail from '../Component/Cobot_components/CobotDetail';
 import { ThemeProvider } from '../contexts/ThemeContext';
 import HeroSection from '../Component/Cobot_components/heroSection';
+import A2ComparisonChart from '../Component/Cobot_components/CobotComparisonChart';
 
 function Cobot_page() {
   return (
@@ -13,6 +14,7 @@ function Cobot_page() {
         <HeroSection/>
         <CobotVariants/>
         <CobotDetail/>
+        <A2ComparisonChart />
         <Footer />
       </div>
     </ThemeProvider>
