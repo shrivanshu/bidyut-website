bb69a1d2ed7d6b378dfb613042403e627aa76cb3
Roshani Sahu <roshani032003@gmail.com>
2025-12-09 14:50:11 +0530

    commit

--- frontend/src/Component/home_components/heroSection.tsx
+++ frontend/src/Component/home_components/heroSection.tsx
@@ -184,7 +184,11 @@ const HeroSection: React.FC = () => {
 
         {/* Description */}
         <p className="text-white text-base sm:text-lg max-w-2xl mx-auto leading-relaxed drop-shadow-md px-2 sm:px-4">
-          Bidyut is the country's most advanced Robotic EdTech Company, empowering schools and students in their quest for holistic development. We offer hands-on robotics education and robotics courses in India that integrate coding solutions, AI learning, and STREAM labs to help students become future-ready.
+          Bidyut is the country's most advanced <a
+  href="/About"
+  target="_blank"
+  rel="noopener noreferrer"
+> Robotic EdTech Company </a>, empowering schools and students in their quest for holistic development. We offer hands-on robotics education and robotics courses in India that integrate coding solutions, AI learning, and STREAM labs to help students become future-ready.
 
         </p>
       </div>
